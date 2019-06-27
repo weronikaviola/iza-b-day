b7e6498b4524b676b9737b8caa36976fbb54821f
Wojciech Ozga <wojciech.ozga@outlook.com>
2019-06-27 10:45:48 +0200

    advanced badguys: new styling, IQ, and speed

--- javascript/bad_man.js
+++ javascript/bad_man.js
@@ -7,41 +7,41 @@ class BadMan {
 		this.posY = posY;
 		this.moving = false;
 		this.rowMap = map;
-		this.id = id; 
+		this.id = id;
 	}
 
-	get type() { return "BadMan"; }	
+	get type() { return "BadMan"; }
 
 	static create(x, y, rows) {
 		return new BadMan(x * SQUARE_SIZE, y * SQUARE_SIZE, rows);
 	}
 }
 
-BadMan.prototype.speed = function() {
+BadMan.prototype.speed = function () {
 	if (this.id == 0) {
-		return 1250;
+		return 1000;
 	} else if (this.id == 1) {
-		return 850;
+		return 500;
 	} else if (this.id == 2) {
-		return 400;
+		return 200;
 	} else {
-		return 450;
+		return 600;
 	}
 }
 
-BadMan.prototype.iq = function() {
+BadMan.prototype.iq = function () {
 	if (this.id == 0) {
-		return 3;
+		return 20;
 	} else if (this.id == 1) {
-		return 5;
+		return 10;
 	} else if (this.id == 2) {
 		return 8;
 	} else {
-		return 4;
+		return 12;
 	}
 }
 
-BadMan.prototype.icon = function() {
+BadMan.prototype.icon = function () {
 	if (this.id == 0) {
 		return "images/bp.png";
 	} else if (this.id == 1) {
@@ -49,82 +49,91 @@ BadMan.prototype.icon = function() {
 	} else if (this.id == 2) {
 		return "images/nestle80x80.png";
 	} else {
-		return "images/bad-kitty.png";
+		return "images/danone.png";
 	}
 }
 
-BadMan.prototype.findBestMove = function (water, posX, posY, previousMove, moves) {
+BadMan.prototype.findBestMove = function (water, posX, posY, moves) {
 	if (water.findIndex(l => (l.read().x == posX && l.read().y == posY)) >= 0) {
 		// found the water, return the best moves
-		return [0, moves];
+		return [10 * moves.length, moves];
 	} else if (moves.length > this.iq()) {
-		// the badguys analyze just few move in forward (IQ defines it)
+		// the badguys analyze just few moves in forward (IQ defines it)
 		let minDistance = null;
 		water.forEach(function (drop, index) {
-			// the metric is the euclides distance between the BadGuy and the closes WaterDrop
 			// the lower the value is, better
-			let distance = Math.abs(drop.read().x - posX) + Math.abs(drop.read().y - posY);
-			if (distance == null || minDistance > distance) {
+			let distance = Math.abs(drop.read().x / SQUARE_SIZE - posX / SQUARE_SIZE) + Math.abs(drop.read().y / SQUARE_SIZE - posY / SQUARE_SIZE);
+			if (minDistance == null || minDistance > distance) {
 				minDistance = distance;
 			}
-		}); 
-		return [minDistance, moves];
+		});
+		return [10 * moves.length + minDistance, moves];
 	} else {
 		// make sure we do not go back
+		let previousMove = moves[moves.length - 1]
 		let distance = null;
 		let bestMoves = []
 		if (previousMove != "left" && this.canMove(posX, posY, "right")) {
 			newMoves = moves.slice(0);
 			newMoves.push("right");
-			let values = this.findBestMove(water, posX + SPEED, posY, "right", newMoves);
-			let newDistance = values[0]
-			let newBestMoves = values[1]
-			if (distance == null || newDistance < distance) {
-				distance = newDistance
-				bestMoves = newBestMoves
+			let newBestMoves = this.findBestMove(water, posX + SPEED, posY, newMoves);
+			if (newBestMoves != null && (distance == null || newBestMoves[0] < distance)) {
+				distance = newBestMoves[0]
+				bestMoves = newBestMoves[1]
 			}
 		}
 		if (previousMove != "right" && this.canMove(posX, posY, "left")) {
 			newMoves = moves.slice(0);
 			newMoves.push("left");
-			let values = this.findBestMove(water, posX - SPEED, posY, "left", newMoves);
-			let newDistance = values[0]
-			let newBestMoves = values[1]
-			if (distance == null || newDistance < distance) {
-				distance = newDistance
-				bestMoves = newBestMoves
+			let newBestMoves = this.findBestMove(water, posX - SPEED, posY, newMoves);
+			if (newBestMoves != null && (distance == null || newBestMoves[0] < distance)) {
+				distance = newBestMoves[0]
+				bestMoves = newBestMoves[1]
 			}
 		}
 		if (previousMove != "down" && this.canMove(posX, posY, "up")) {
 			newMoves = moves.slice(0);
 			newMoves.push("up");
-			let values = this.findBestMove(water, posX, posY - SPEED, "up", newMoves);
-			let newDistance = values[0]
-			let newBestMoves = values[1]
-			if (distance == null || newDistance < distance) {
-				distance = newDistance
-				bestMoves = newBestMoves
+			let newBestMoves = this.findBestMove(water, posX, posY - SPEED, newMoves);
+			if (newBestMoves != null && (distance == null || newBestMoves[0] < distance)) {
+				distance = newBestMoves[0]
+				bestMoves = newBestMoves[1]
 			}
 		}
 		if (previousMove != "up" && this.canMove(posX, posY, "down")) {
 			newMoves = moves.slice(0);
 			newMoves.push("down");
-			let values = this.findBestMove(water, posX, posY + SPEED, "down", newMoves);
-			let newDistance = values[0]
-			let newBestMoves = values[1]
-			if (distance == null || newDistance < distance) {
-				distance = newDistance
-				bestMoves = newBestMoves
+			let newBestMoves = this.findBestMove(water, posX, posY + SPEED, newMoves);
+			if (newBestMoves != null && (distance == null || newBestMoves[0] < distance)) {
+				distance = newBestMoves[0]
+				bestMoves = newBestMoves[1]
 			}
 		}
-		let result = [].concat(moves, bestMoves)
-		return [distance, result]
+		if (distance == null) {
+			// we could reach a corner from which the only way back is to actually go back
+			return null
+		}
+		return [distance, bestMoves]
 	}
 }
 
-BadMan.prototype.move = function (water, previousMove) {
-	let values = this.findBestMove(water, this.posX, this.posY, previousMove, []);
-	let direction = values[1][0]
+BadMan.prototype.move = function (water) {
+	let moves = this.findBestMove(water, this.posX, this.posY, []);
+	if (moves == null) {
+		// we have wall around us. do nothing
+		return
+	}
+	let direction = moves[1][0]
+	if (this.id == 2) {
+		console.log("###########")
+		console.log("Current location: ", this.posX / SQUARE_SIZE, this.posY / SQUARE_SIZE)
+		console.log("Move to ", direction)
+		console.log("BestMoves", moves)
+		water.forEach(function (item, index) {
+			console.log("Water: ", item.read().x / SQUARE_SIZE, item.read().y / SQUARE_SIZE);
+		});
+		console.log("###########")
+	}
 
 	if (direction == "right") {
 		this.posX = this.posX += SPEED;
